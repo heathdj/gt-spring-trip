9400fd0fcd050acaffe6095c981985e5cbaf657c
David Heath <heathdj@gmail.com>
2019-08-25 13:14:07 -0700

    Added in default files

---
+++ src/main/java/com/geektrek/trip/GtSpringTripApplication.java
@@ -0,0 +1,13 @@
+package com.geektrek.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtSpringTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtSpringTripApplication.class, args);
+    }
+
+}
